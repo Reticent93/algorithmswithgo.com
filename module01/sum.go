6c154c6fff467a5bb5654be0bc22886ae71dc883
Greg <remph819@gmail.com>
2020-09-10 10:26:59 -0700

    test and pass num in list

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,17 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	total := 0
+	for _, val := range numbers {
+		total += val
+	}
+	return total
 }
+
+//Recursion way
+//func Sum(numbers []int) int  {
+//	if len(numbers) == 0 {
+//		return 0
+//	}
+//	return numbers[0] + Sum(numbers[1:])
+//}
